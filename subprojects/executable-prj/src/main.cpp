2cd653e6652f72992d75f39eeeab443ce116157d
Kerosin3 <keromvp@gmail.com>
2024-04-20 21:34:38 +0300

    added details

--- subprojects/executable-prj/src/main.cpp
+++ subprojects/executable-prj/src/main.cpp
@@ -9,16 +9,23 @@
 #include <sstream>
 #include <thread>
 
+#include <boost/bimap.hpp>
+#include <boost/bimap/multiset_of.hpp>
+#include <boost/program_options.hpp>
+
 #include "libexample/example.hpp"
 #include "test1.hpp"
+namespace po = boost::program_options;
 
 int
 main(int argc, char* argv[])
 {
-  Myspace::Container Container1 {10};
-  Container1.computeC(10);
-  std::cout << Container1 << std::endl;
-  auto Add = Container1 + 5;
-  std::cout << Add << std::endl;
+  MyArray::Xtest<double, 10> MyArray {};
+  MyArray.printout();
+  MyArray::Ytest<int, 10> MyArray1 {};
+  MyArray1.printout();
+  std::cout << "for Y specialized" << std::endl;
+  MyArray::Ytest<double, 10> MyArray2 {};
+  MyArray2.printout();
   return EXIT_SUCCESS;
 }
--- subprojects/libexample/headers/libexample/example.hpp
+++ subprojects/libexample/headers/libexample/example.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <array>
 #include <cstddef>
 #include <format>
 #include <iostream>
@@ -11,28 +12,74 @@
 
 namespace Myspace
 {
-class Container
-{
-public:
-  Container() = default;
-  ~Container() = default;
-  explicit Container(int t_par1)
-      : m_result(t_par1) {};
-
-  void computeC(int t_arg)
+  class Container
   {
-    if (!t_arg) {
-      m_result = std::nullopt;
-    } else {
-      m_result = t_arg;
+  public:
+    Container() = default;
+    ~Container() = default;
+    explicit Container(int t_par1)
+        : m_result(t_par1) {};
+
+    void computeC(int t_arg)
+    {
+      if (!t_arg) {
+        m_result = std::nullopt;
+      } else {
+        m_result = t_arg;
+      }
     }
-  }
 
-  friend std::ostream& operator<<(std::ostream& out, const Container& t_par);
-  friend Container operator+(const Container&, int rhs);
+    friend std::ostream& operator<<(std::ostream& out, const Container& t_par);
+    friend Container operator+(const Container&, int rhs);
 
-private:
-  std::optional<int> m_result;
-};
+  private:
+    std::optional<int> m_result;
+  };
 
 }  // namespace Myspace
+
+namespace MyArray
+{
+
+  template<typename U, int size>
+  class Xtest
+  {
+  public:
+    Xtest<U, size>()
+    {
+      for (int count {0}; count < size; ++count) {
+        m_array[count] = count;
+      }
+    }
+
+    void printout() const noexcept
+    {
+      for (int count {0}; count < size; ++count) {
+        std::cout << "b value is " << m_array[count] << std::endl;
+      }
+    }
+
+  protected:
+    std::array<U, size> m_array {};
+  };
+
+  // inherite general case
+  template<typename U, int size>
+  class Ytest : virtual public Xtest<U, size>
+  {
+  };
+
+  // specialize for double
+  template<int size>
+  class Ytest<double, size> : virtual public Xtest<double, size>
+
+  {
+  public:
+    void printout() const noexcept
+    {
+      for (int count {0}; count < size; ++count) {
+        std::cout << "OV value is " << this->m_array[count] << std::endl;
+      }
+    };
+  };
+}  // namespace MyArray
